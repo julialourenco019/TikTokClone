e52e39a79693a004a5d94c3c831635aaaa8c2f08
julia <julialourenco1901@outlook.com>
2023-03-30 18:37:17 -0300

    primeira alteração

---
+++ src/pages/Video.js
@@ -0,0 +1,35 @@
+import React ,{ useRef, useState} from 'react'
+import  "./Video.css"
+
+function Video() {
+  const VideoRef = useRef(null)
+  const  [play, setPlay] = useState(false)
+
+  function handdleStart(){
+   if(play){
+      VideoRef.current.pause()
+      setPlay(false)
+    }
+    else{
+      VideoRef.current.play()
+      setPlay(true)
+   }
+    }
+  return (
+    <div className='Video'>
+       
+    <video 
+       className='video__player'
+       ref={VideoRef}
+       onClick={handdleStart}
+       loop
+       src="
+       https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4">
+       
+
+        </video>
+</div>
+  )
+}
+
+export default Video
